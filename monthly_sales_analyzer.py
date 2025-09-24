a793139a104f811f4d1c95faf5cab5f6e3da380d
Adam Candalija Naranjo <adamcandalija@gmail.com>
2025-09-24 19:22:57 +0000

    comentario

--- monthly_sales_analyzer.py
+++ monthly_sales_analyzer.py
@@ -94,6 +94,4 @@ print("Total sales of product_a:", total_sales_by_product(sales_data, "product_a
 print("Average daily sales of product_b:", average_daily_sales(sales_data, "product_b"))
 print("Day with highest total sales:", best_selling_day(sales_data))
 print("Days when product_c exceeded 300 sales:", days_above_threshold(sales_data, "product_c", 300))
-print("Product with highest total sales:", top_product(sales_data))
-
-#prueba
+print("Product with highest total sales:", top_product(sales_data)
